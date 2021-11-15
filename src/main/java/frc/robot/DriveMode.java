d87af54bc75e1a582171e6292c9ee02f5470cbfa
innyun <vinnysun1@gmail.com>
2021-11-15 16:20:01 -0600

    initial commit

---
+++ src/main/java/frc/robot/DriveMode.java
@@ -0,0 +1,6 @@
+package frc.robot;
+
+public enum DriveMode {
+    DRIVERCONTROL,
+    AUTODRIVING
+}
---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,80 @@
+package frc.robot;
+
+import edu.wpi.first.hal.HAL;
+import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class Robot extends RobotBase {
+
+    private IRobotMode disabledMode;
+    private IRobotMode autonomousMode;
+    private IRobotMode teleoperatedMode;
+
+    private IDrive drive;
+    private IGyroscopeSensor gyroscope;
+
+    public Robot() {
+        gyroscope = new NavXMXP();
+
+        drive = new Drive(gyroscope);
+        //drive = new NullDrive();
+        disabledMode = new DisabledMode();
+        autonomousMode = new AutonomousMode(drive);
+        teleoperatedMode = new TeleoperatedMode(drive);
+    }
+
+
+    @Override
+    public void startCompetition() {
+        HAL.observeUserProgramStarting();
+
+        IRobotMode currentMode = null;
+        IRobotMode desiredMode = null;
+
+        while (true) {
+            LiveWindow.setEnabled(isTest());
+            doPeripheralPeriodicProcessing();
+            desiredMode.init();
+            currentMode = desiredMode;
+        }
+        currentMode.periodic();
+        doPeripheralPeriodicProcessing();
+        SmartDashboard.updateValues();
+        LiveWindow.updateValues();
+    }
+
+    private void doPeripheralReinitialization() {
+        drive.init();
+    }
+
+    private void doPeripheralPeriodicProcessing() {
+        drive.periodic();
+
+        Debug.periodic();
+    }
+
+    private IRobotMode getDesiredMode() {
+        if (isDisabled()) {
+            HAL.observeUserProgramDisabled();
+            return disabledMode;
+        } else if (isAutonomous()) {
+            HAL.observeUserProgramAutonomous();
+            return autonomousMode;
+        } else if (isOperatorControl()) {
+            HAL.observeUserProgramTeleop();
+            return teleoperatedMode;
+        } else if (isTest()) {
+            HAL.observeUserProgramTest();
+            return teleoperatedMode;
+        } else {
+            throw new IllegalStateException("Robot is in an invalid mode");
+        }
+    }
+
+    @Override
+    public void endCompetition() {
+        //TODO: add end of competition code 
+    }
+}
