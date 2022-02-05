7a88c8091103a6281c3edb3104a71b223b651cab
innyun <56566275+innyun@users.noreply.github.com>
2022-02-05 15:48:35 -0600

    changed deprecated method in Robot (#8)

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -66,7 +66,7 @@ public class Robot extends RobotBase {
         } else if (isAutonomous()) {
             HAL.observeUserProgramAutonomous();
             return autonomousMode;
-        } else if (isOperatorControl()) {
+        } else if (isTeleop()) {
             HAL.observeUserProgramTeleop();
             return teleoperatedMode;
         } else if (isTest()) {
