76056dafc7875941f49aed61a0839e9544add351
allinae-dendough <allidougherty232@gmail.com>
2022-02-15 18:43:57 -0600

    edited errors in rotations and mispellings

--- src/main/java/frc/robot/TeleoperatedMode.java
+++ src/main/java/frc/robot/TeleoperatedMode.java
@@ -1,32 +1,24 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.DriverStation;
 
 public class TeleoperatedMode implements IRobotMode {
     
-    private XboxController XboxController;
+    private XboxController xboxController;
     private IDrive drive;
-    private ILaucher laucher;
-    private ILimelight limelight;
-    private IColorWheel wheel;
-
-    private String data;
-    private double distanceFromTarget;
+    private ILauncher launcher;
 
     private static final double LEFT_STICK_EXPONENT = 3.0;
     private static final double RIGHT_STICK_EXPONENT = 3.0;
-    private static final double ROTATION_THRESOLD = 0.3;
+    private static final double ROTATION_THRESHOLD = 0.3;
     // check values above
 
-    public TeleoperatedMode(IDrive drive, ILauncher launcher, ILimelight limelight, IColorWheel wheel){ 
-        xboxController = new XboxController(*****);
+    public TeleoperatedMode(IDrive drive, ILauncher launcher){ 
+        xboxController = new XboxController(PortMap.USB.XBOXCONTROLLER);
 
         this.drive = drive;
-        this.laucher = launcher;
-        this.limelight = limelight;
-        this.wheel = wheel;
+        this.launcher = launcher;
     }
 
     @Override
@@ -37,10 +29,43 @@ public class TeleoperatedMode implements IRobotMode {
      @Override
      public void periodic(){
 
-        double leftX = xboxControllers.getx(Hand.kleft);
-        double leftY = -xboxController.getY(Hand.kLeft);
+        double leftX = xboxController.getLeftX();
+        double leftY = -xboxController.getLeftY();
 
-        leftX = Math.pow(leftX, LEFT_STICK_EXPONENT)
+        leftX = Math.pow(leftX, LEFT_STICK_EXPONENT);
+        leftY = Math.pow(leftY, LEFT_STICK_EXPONENT);
+
+        drive.driveManual(leftY, leftX);
+
+        //Process Rotation Control
+        double rightX = xboxController.getRightX();
+        double rightY = -xboxController.getRightY();
+
+        rightX = Math.pow(rightX, RIGHT_STICK_EXPONENT);
+        rightY = Math.pow(rightY, RIGHT_STICK_EXPONENT);
+
+        double angle = Math.atan2(rightX, rightY);
+
+        // Think Pythagorean Thereom
+        if(Math.sqrt(Math.pow(rightX, 2) + Math.pow(rightY, 2)) > ROTATION_THRESHOLD) {
+            drive.rotateAbsolute(angle);
+        }
+         // Process Peripheral control
+         if (xboxController.getRightBumper()){
+            launcher.shoot();
+        }
+
+        if (xboxController.getLeftBumper()) {
+            launcher.intake();
+        }
+
+        if (xboxController.getAButton()) {
+            launcher.reverse();
+        }
+
+        if (xboxController.getBButton()) {
+            launcher.advance();
+        }
      }
 
 }
