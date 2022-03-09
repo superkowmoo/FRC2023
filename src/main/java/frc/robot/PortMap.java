eb52ce19d85eeb7615e01ca5a2233fe0bdc7ec07
innyun <vinnysun1@gmail.com>
2022-03-08 20:22:38 -0600

    fixed syntax errors; added endgame to Portmap

--- src/main/java/frc/robot/PortMap.java
+++ src/main/java/frc/robot/PortMap.java
@@ -11,10 +11,11 @@ public class PortMap {
         public static final int FRONT_RIGHT_MOTOR = 2;
         public static final int REAR_LEFT_MOTOR = 4;
         public static final int REAR_RIGHT_MOTOR = 1;
-        public static final int INTAKE_MOTOR_CONTROLLER = 8;
-        public static final int WHEEL_MOTOR_CONTROLLER = 20;
-        public static final int SHOOTER_MOTOR_CONTROLLER = 10;
-        public static final int STORAGE_MOTOR_CONTROLLER = 7;
+        public static final int INTAKE_MOTOR_CONTROLLER = 5;
+        public static final int SHOOTER_MOTOR_CONTROLLER = 7;
+        public static final int STORAGE_MOTOR_CONTROLLER = 6;
+        public static final int ENDGAME_RIGHT_MOTOR = 9;
+        public static final int ENDGAME_LEFT_MOTOR = 8;
     }
 
     public class DIO {
